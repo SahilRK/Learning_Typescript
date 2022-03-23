167d68171e399c3660f0c4c8213a30307a0d7fd7
Sahil_Kulkarni <sahil.rordev@gmail.com>
2022-03-23 13:04:49 +0530

    Added examples of annotations

---
+++ annotations/variables.ts
@@ -0,0 +1,23 @@
+//Type annotations
+//It is the code we write to tell typescript what type of value a variables should store
+
+//Data type annotations
+//Number annotation
+let age: number = 10;
+
+//String annotation
+let perName: string = "Sahil";
+
+//Boolean annotation
+let isWorking: boolean = true;
+
+//Data structure annotations
+let colours: string[] = ["Red","Yellow","Green"];
+
+let ages: number[] = [31,22,29];
+
+class Person{
+
+}
+
+let person1: Person = new Person();
